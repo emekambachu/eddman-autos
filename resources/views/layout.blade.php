fbf0f010d3e259bb110ccb0ddcee3ed3113baad4
Emeka Victor Mbachu <32366820+emekambachu@users.noreply.github.com>
2022-05-19 16:09:38 +0100

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -243,19 +243,7 @@
 
 @yield('bottom-assets')
 
-<!--Start of Tawk.to Script-->
-<script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/61a3bf4b9099530957f7042b/1fljq6gc3';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-    })();
-</script>
-<!--End of Tawk.to Script-->
+<script src="//code.tidio.co/7lljtsaxmruhetx4w3tks61e7dbcjyvo.js" async></script>
 
 </body>
 
